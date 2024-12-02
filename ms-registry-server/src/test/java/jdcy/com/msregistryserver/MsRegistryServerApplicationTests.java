9e200593ab1b25d66fbe70efdc1ec11861cf9c64
DanielPOO <ccanadaniely@gmail.com>
2024-12-02 12:35:51 -0500

    registry y gateway

---
+++ ms-registry-server/src/test/java/jdcy/com/msregistryserver/MsRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jdcy.com.msregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
