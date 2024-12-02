952ecde1ec9ebdcf57b5753f4983f9e86143272e
DanielPOO <ccanadaniely@gmail.com>
2024-12-01 22:56:47 -0500

    cambio

--- ms-category/src/main/java/jdcy/com/mscategory/MsCategoryApplication.java
+++ ms-category/src/main/java/jdcy/com/mscategory/MsCategoryApplication.java
@@ -17,7 +17,7 @@ public class MsCategoryApplication {
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI().info(new Info()
-				.title("OPEN API MICROSERVICIO CATÁLOGO")
+				.title("OPEN API MICROSERVICIO CATEGORIA")
 				.version("0.0.1")
 				.description("servicios web catálogo")
 				.termsOfService("http:// swagger.io/terms")
